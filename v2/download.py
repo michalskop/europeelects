23d78f6259d83fd5341a6a01c99e1598b78e14c1
Michal Skop <michal.skop@kohovolit.eu>
2024-01-08 00:29:55 +0100

    v2 download

---
+++ v2/download.py
@@ -0,0 +1,59 @@
+"""Download data from EuropeElects.eu."""
+
+import csv
+import requests
+import requests_html
+import time
+
+# local path
+localpath = "v2/"
+
+# overview table url
+url0 = "https://europeelects.eu/data/"
+
+# get the table
+session = requests_html.HTMLSession()
+r = session.get(url0)
+r.html.render()
+
+if r.status_code != 200:
+  raise Exception("Could not download data.")
+
+# select table
+table = r.html.find("figure[class=wp-block-table]")[0].find("table")[0]
+
+# get data
+data = []
+for row in table.find('tr'):
+  cells = row.find('td')
+  print(cells)
+  if cells:
+    country = cells[0].find('a', first=True)
+    country_name = country.text if country else None
+    country_link = country.attrs['href'] if country else None
+    try:
+      data_link = cells[1].find('a', first=True).attrs['href']
+      country_code = data_link.split("/")[-1].split(".")[0]
+    except:
+      data_link = None
+    if country_name and country_link and data_link:
+      data.append({
+        "country_code": country_code,
+        "country_name": country_name, 
+        "country_link": country_link,
+        "data_link": data_link
+      })
+
+# write data to csv
+with open(localpath + "list.csv", "w") as f:
+  writer = csv.DictWriter(f, fieldnames=data[0].keys())
+  writer.writeheader()
+  writer.writerows(data)
+
+# download data
+for d in data:
+  r = requests.get(d["data_link"])
+  if r.status_code == 200:
+    with open(localpath + "data/" + d["country_code"] + ".csv", "wb") as f:
+      f.write(r.content)
+  time.sleep(1)
